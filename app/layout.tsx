ae5987b3f1ead6df54a734b72c503b3efba86f3e
Mahmoud Abuyoussef <mahmoudabuyoussef20@gmail.com>
2024-02-28 05:42:20 +0200

    Add Url

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,37 +31,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff" />
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
-
         <meta property="og:title" content="Weather Facts Mahmoud Abuyoussef" />
         <meta property="og:description" content="Get the most accurate weather data with our advanced Weather Facts. Powered by hyper-local forecasts and real-time updates" />
-
-        <meta property="og:url" content="" />
-
+        <meta property="og:url" content="https://weather-facts.vercel.app/" />
         <meta property="og:site_name" content="Weather Facts Mahmoud Abuyoussef" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="ar-EG" />
-        <meta property="og:image" content="/android-chrome-192x192.png" />
-
+        <meta property="og:image" content="https://weather-facts.vercel.app/android-chrome-192x192.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content="Weather Facts Mahmoud Abuyoussef" />
         <meta property="twitter:description" content="Get the most accurate weather data with our advanced Weather Facts. Powered by hyper-local forecasts and real-time updates" />
-
-        <meta property="twitter:url" content="" />
-
+        <meta property="twitter:url" content="https://weather-facts.vercel.app/" />
         <meta property="twitter:site_name" content="Weather Facts Mahmoud Abuyoussef" />
         <meta property="twitter:type" content="website" />
         <meta property="twitter:locale" content="ar-EG" />
-        <meta property="twitter:image" content="/android-chrome-192x192.png" />
-
-        <link rel="canonical" href="" />
-
+        <meta property="twitter:image" content="https://weather-facts.vercel.app/android-chrome-192x192.png" />
+        <link rel="canonical" href="https://weather-facts.vercel.app/" />
         <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="android-chrome-512x512.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="android-chrome-192x192.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
         <link rel="icon" href="favicon.ico" />
-        <link rel="mask-icon" href="assets/icons/mask/maskable_icon_x72.png" color="#ffffff" />
         <meta name="msapplication-TileColor" content="#ffffff" />
       </head>
       <body className={poppins.className}>{children}</body>
